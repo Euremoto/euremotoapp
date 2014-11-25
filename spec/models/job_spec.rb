97c6ae7f3d81c6131b2ca6024d1a4748e8bb2e09
Breno Oliveira <breno26@gmail.com>
2014-11-25 20:24:44 -0200

    adiciona unicorn

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Job, :type => :model do
   describe ".confirmed?" do
     let(:confirmed) { FactoryGirl.build(:confirmed_job) }
     it do
-      # pry.bindding
       expect(confirmed).to be_confirmed
     end
   end
